2f3141fc741d01e997e9819df5ccbb3060658cc0
AmanTrechsel <112391335+AmanTrechsel@users.noreply.github.com>
2024-08-13 10:07:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-wzcngq
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/AmanTrechsel/vitejs-vite-wzcngq)
---
+++ app/src/components/back.js
@@ -0,0 +1,7 @@
+export default function Back() {
+  return (
+    <button class="back" type="button">
+      <img source="../../../assets/back.svg"
+    </button>
+  )
+}
